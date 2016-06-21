2a658a5de211b44e5ed41a13b67d1d7b944af95d
wenjun001 <wenjunma001@gmail.com>
2016-06-20 20:15:25 -0700

    add SFS template

---
+++ src/template/DFSTemplate.java
@@ -0,0 +1,50 @@
+package template;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xiu on 6/20/16.
+ */
+public class DFSTemplate {
+
+    public static void main(String[] args) {
+
+        String[] source = {"a","b"};
+        List<List<String>> res = new ArrayList<>();
+        List<String> temp = new ArrayList<>();
+
+        dfs(source,res,temp,0,source.length);
+        int i = 0;
+        for (List<String> root:res
+                ) {
+            System.out.print("i:"+i++ +"  ");
+            for (String s:root
+                    ) {
+                System.out.print(s);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void dfs(String[] source,List<List<String>> res,List<String> temp, int start,int size){
+        // return exist
+        if(start>size){
+            return;
+        }
+        // add result
+        res.add(new ArrayList<String>(temp));
+
+        for(int i = start;i<size;i++){
+            // add path
+            temp.add(source[i]);
+            dfs(source,res,temp,i+1,size);
+            // remove path
+            temp.remove(source[i]);
+        }
+
+    }
+
+
+}
